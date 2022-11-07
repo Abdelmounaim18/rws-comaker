009ce52dae4a2b6ea0352c44b7575599c26ebe95
abdullahaltun70 <a.altun70@outlook.com>
2022-11-07 20:13:07 +0100

    start with api and start with json > postgresql

--- dashboard/api/json_to_postgres.py
+++ dashboard/api/json_to_postgres.py
@@ -1,16 +1,54 @@
-# import psycopg2
-# import psycopg2.extras
-# import psycopg2.extensions
-# import pandas as pd
-# import json
+import psycopg2
+import psycopg2.extras
+import psycopg2.extensions
+import pandas as pd
+import json
+
+conn = psycopg2.connect("dbname=rws user=postgres password=altun")
+
+cur = conn.cursor()
+
+conn.autocommit = True
+
+
+# def create_staging_table(cursor):
+#     cursor.execute("""
+#     DROP TABLE IF EXISTS ndw_data CASCADE;
+#     CREATE UNLOGGED TABLE ndw_data(
+#     ndw_data            jsonb
+#     );""")
 #
-# conn = psycopg2.connect("dbname=rws user=postgres password=altun")
 #
-# cur = conn.cursor()
-#
-# conn.autocommit = True
-#
-#
+# with conn.cursor() as cursor:
+#     create_staging_table(cursor)
+
+converted_ndw_data_json = pd.read_json("../data/converted_data/refactored_ndw_data_1.json")  # read json file
+df = converted_ndw_data_json  # create dataframe
+df = df.to_json()
+# df['events'] = list(map(lambda x: json.dumps(x), df['events']))  # add a new column with the same value as the row
+# df['events'] = json.dumps(df['events'], indent=4)
+# df['events'] = json.dumps(df['events'])
+
+# print(df)
+
+def insert_data(df, table, cur):
+    if len(df) > 0:
+        df_columns = list(df)
+        # create (col1,col2,...)
+        columns = ",".join(df_columns)
+
+        # create VALUES('%s', '%s",...) one '%s' per column
+        values = "VALUES({})".format(",".join(["%s"]))
+
+        # create INSERT INTO table (columns) VALUES('%s',...)
+        insert_stmt = "INSERT INTO {} ({}) {}".format(table, columns, values)
+        cur.execute("truncate " + table + ";")  # avoid duplicates
+        psycopg2.extras.execute_batch(cur, insert_stmt, df)
+    conn.commit()
+
+
+insert_data(df, 'ndw_data', cur)
+
 # def create_staging_table_events(cursor):
 #     cursor.execute("""
 #     DROP TABLE IF EXISTS events CASCADE;
@@ -20,8 +58,8 @@
 #     measuring_point_id          TEXT,
 #     uuid                        TEXT
 #     );""")
-
-
+#
+#
 # def create_staging_table_lanelocation(cursor):
 #     cursor.execute("""
 #     DROP TABLE IF EXISTS lanelocation CASCADE;
@@ -31,8 +69,8 @@
 #     lane                        TEXT,
 #     km                          TEXT
 #     );""")
-
-
+#
+#
 # def create_staging_table_roads(cursor):
 #     cursor.execute("""
 #     DROP TABLE IF EXISTS lanelocation CASCADE;
@@ -42,8 +80,8 @@
 #     lane                        TEXT,
 #     km                          TEXT
 #     );""")
-
-
+#
+#
 # with conn.cursor() as cursor:
 #     create_staging_table_lanelocation(cursor)
 #     create_staging_table_events(cursor)
